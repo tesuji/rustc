46a0eadf93230c8b1a344aa5bee14cefe80d2202
Lzu Tao <taolzu@gmail.com>
2024-06-11 07:09:15 +0000

    wip

--- compiler/rustc_mir_transform/src/gvn.rs
+++ compiler/rustc_mir_transform/src/gvn.rs
@@ -367,7 +367,9 @@ impl<'body, 'tcx> VnState<'body, 'tcx> {
     #[instrument(level = "trace", skip(self), ret)]
     fn eval_to_const(&mut self, value: VnIndex) -> Option<OpTy<'tcx>> {
         use Value::*;
-        let op = match *self.get(value) {
+        let vvalue = self.get(value);
+        debug!(?vvalue);
+        let op = match *vvalue {
             Opaque(_) => return None,
             // Do not bother evaluating repeat expressions. This would uselessly consume memory.
             Repeat(..) => return None,
@@ -376,10 +378,11 @@ impl<'body, 'tcx> VnState<'body, 'tcx> {
                 self.ecx.eval_mir_constant(value, DUMMY_SP, None).ok()?
             }
             Aggregate(kind, variant, ref fields) => {
+                debug!(?kind, ?variant, ?fields);
                 let fields = fields
                     .iter()
                     .map(|&f| self.evaluated[f].as_ref())
-                    .collect::<Option<Vec<_>>>()?;
+                    .collect::<Option<Vec<&OpTy<'_>>>>()?;
                 let ty = match kind {
                     AggregateTy::Array => {
                         assert!(fields.len() > 0);
@@ -407,6 +410,24 @@ impl<'body, 'tcx> VnState<'body, 'tcx> {
                     };
                     let ptr_imm = Immediate::new_pointer_with_meta(data, meta, &self.ecx);
                     ImmTy::from_immediate(ptr_imm, ty).into()
+                // } else if matches!(kind, AggregateTy::Array) {
+                //     let mut mplace = None;
+                //     let alloc_id = self.ecx.intern_with_temp_alloc(ty, |ecx, dest| {
+                //         for (field_index, op) in fields.iter().copied().enumerate() {
+                //             let field_dest = ecx.project_field(dest, field_index)?;
+                //             ecx.copy_op(op, &field_dest)?;
+                //         }
+
+                //         let place = dest.assert_mem_place();
+                //         mplace.replace(place);
+                //         Ok(())
+                //     }).ok()?;
+                //     let GlobalAlloc::Memory(_alloc) = self.tcx.global_alloc(alloc_id) else {
+                //         bug!()
+                //     };
+                //     let mplace = mplace.unwrap();
+                //     debug!(?mplace);
+                //     return Some(mplace.into());
                 } else if matches!(ty.abi, Abi::Scalar(..) | Abi::ScalarPair(..)) {
                     let dest = self.ecx.allocate(ty, MemoryKind::Stack).ok()?;
                     let variant_dest = if let Some(variant) = variant {
@@ -429,6 +450,7 @@ impl<'body, 'tcx> VnState<'body, 'tcx> {
             }
 
             Projection(base, elem) => {
+                debug!(?base, ?elem);
                 let value = self.evaluated[base].as_ref()?;
                 let elem = match elem {
                     ProjectionElem::Deref => ProjectionElem::Deref,
@@ -450,6 +472,7 @@ impl<'body, 'tcx> VnState<'body, 'tcx> {
                 self.ecx.project(value, elem).ok()?
             }
             Address { place, kind, provenance: _ } => {
+                debug!(?place, ?kind);
                 if !place.is_indirect_first_projection() {
                     return None;
                 }
